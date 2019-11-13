3c7b54ee66c3539435bdcc7791230cfa72808943
Ewerton Duarte <joaosouza@alunos.utfpr.edu.br>
2019-11-12 23:49:25 -0300

    script para a imputação de dados e teste com modelos lineares

---
+++ missing_script.r
@@ -0,0 +1,36 @@
+install.packages("mice") #pacote que contem função para imputação de dados
+
+ob <- read.csv2("observacao.csv", sep=",", dec = ".")
+
+library(mice, warn.conflicts = FALSE) #carrega a lib mice
+#md.pattern(ob)
+
+tempData <- mice(ob,m=1,maxit=50,meth='cart',seed=500) # função para imputação de dados
+
+summary(tempData)
+
+
+#ob[1,5] <- NA
+#tempData$imp$relevo_elevacao
+imputeData <- complete(tempData) #extração da nova tabela com dados imputados]
+
+################## MODELOS #########################
+model_x <- lm(formula= coord_x ~ relevo_elevacao +
+              taxon_sibcs_2006 + relevo_local + relevo_declividade + relevo_posicao,
+            data=imputeData)
+
+model_y <- lm(formula= coord_x ~ relevo_elevacao +
+              taxon_sibcs_2006 + relevo_local + relevo_declividade + relevo_posicao,
+            data=imputeData)
+
+################## TESTES ##########################
+instance <- data.frame(
+  relevo_elevacao = 300,
+  taxon_sibcs_2006 = "Latossolo Vermelho Distrófico típico, textura média, A moderado",
+  relevo_local = "plano",
+  relevo_declividade = 4,
+  relevo_posicao = "Terço superior"
+)
+
+predict(model_x, newdata = instance)
+predict(model_y, newdata = instance)
