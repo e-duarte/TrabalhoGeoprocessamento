126fb6a9c6b4b1999a1eee17a2ab06bb0623ded9
JOAO EWERTON DUARTE DE SOUZA <a1808567@P-WS-056-L06-LAB-205.sh.utfpr.edu.intranet>
2019-11-27 09:14:28 -0300

    tratamento base

---
+++ tratamento_base.r
@@ -0,0 +1,22 @@
+install.packages('googlesheets')
+install.packages("dplyr")
+
+library(googlesheets)
+library(magrittr)
+library(dplyr)
+
+belavista <- 
+  googlesheets::gs_key('1LMa_n5E2xGnZqKFuhf_8hAG83sdoNm30PgeTVfs25FM') %>%
+  googlesheets::gs_read(ws = 'observacao', na = '-', comment ="#metadado>")
+
+cambe <- 
+  googlesheets::gs_key('1HlFLNRDzRqD42lJ1GF3Pv00xHBMJ2w9Yj4Z2U1OR5_Q') %>%
+  googlesheets::gs_read(ws = 'observacao', na = '-', comment ="#metadado>")
+
+londrina <- 
+  googlesheets::gs_key('1lSxtGUxa6Rqr4wQZYhp8pYg-KDe3soMft8Ki6KX1XxA') %>%
+  googlesheets::gs_read(ws = 'observacao', na = '-', comment ="#metadado>")
+
+obs <- rbind(belavista, cambe, londrina) %>%
+  dplyr::mutate(
+    taxon = stringr::word(taxon_sibcs_2006, sep=',')
