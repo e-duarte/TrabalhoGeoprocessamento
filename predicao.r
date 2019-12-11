53a3f766ba60b7519ddcd3c9273d30e75a8fbe74
ewerton duarte <ewertonduart37@gmail.com>
2019-12-10 22:57:48 -0300

    modelo para londrina

--- predicao.r
+++ predicao.r
@@ -19,11 +19,22 @@ summary(londrina_modely)
 
 
 ####################################### Verificação de Ajuste #####################################
-#summary(modelx)
+# ajuste do modelo todos_modelx e todos_modely
+step(todos_modelx, direction = "both", scale=8192)
+step(todos_modely, direction = "both", scale=10530)
+shapiro.test(todos_modelx$residuals)
+shapiro.test(todos_modely$residuals)
 
-#step(model, direction = "both", scale=7987)
-#summary(adj)
+par(mfrow = c(2,4))
+plot(todos_modelx, which=c(1:4), pch=20)
+plot(todos_modely, which=c(1:4), pch=20)
 
-#shapiro.test(modelx$residuals)
-#par(mfrow = c(2,4))
-#plot(modelx, which=c(1:4), pch=20)
+# ajuste do modelo londrina_modelx e londrina_modely
+step(londrina_modelx, direction = "both", scale=7987)
+step(londrina_modelx, direction = "both", scale=7987)
+shapiro.test(londrina_modelx$residuals)
+shapiro.test(londrina_modely$residuals)
+
+par(mfrow = c(2,4))
+plot(londrina_modelx, which=c(1:4), pch=20)
+plot(londrina_modely, which=c(1:4), pch=20)
